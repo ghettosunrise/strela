8701708dad4774d25fa20225a9c46d12c8b2c838
Vlad <loban1927@gmail.com>
2020-06-15 22:43:52 +0300

    fixed divider bug

--- src/components/Divider/Divider.jsx
+++ src/components/Divider/Divider.jsx
@@ -1,21 +1,19 @@
-import React from "React"
-import Flex from "../../styled/flex"
-import { DividerText, DividerArrow } from "./styles"
+import React from 'react';
+import Flex from '../../styled/flex';
+import { DividerText, DividerArrow } from './styles';
 
-const Divider = props => {
-  return (
-    <Flex
-      width="100%"
-      align="flex-end"
-      row
-      paddingAll="0px 49px"
-      marginBottom="50px"
-      tabletPadding="0 30px"
-    >
-      <DividerText>{props.text}</DividerText>
-      <DividerArrow />
-    </Flex>
-  )
-}
+const Divider = (props) => (
+  <Flex
+    width="100%"
+    align="flex-end"
+    row
+    paddingAll="0px 49px"
+    marginBottom="50px"
+    tabletPadding="0 30px"
+  >
+    <DividerText>{props.text}</DividerText>
+    <DividerArrow />
+  </Flex>
+);
 
-export default Divider
+export default Divider;
