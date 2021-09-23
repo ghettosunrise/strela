b1f5a6dda9aef8d89fbbb80922d1a12c729cf147
Vlad <loban1927@gmail.com>
2021-09-23 21:37:25 +0300

    fix

--- src/components/Buttons/ButtonBlue/ButtonBlue.jsx
+++ src/components/Buttons/ButtonBlue/ButtonBlue.jsx
@@ -2,23 +2,13 @@ import React from 'react';
 import * as S from './styles';
 import Flex from '../../../styled/flex';
 
-const ButtonBlue = ({ pressKit }) => {
-  console.log(
-    '🚀 ~ file: ButtonBlue.jsx ~ line 6 ~ ButtonBlue ~ pressKit',
-    pressKit
-  );
+const ButtonBlue = () => (
+  // const { url } = pressKit.file;
 
-  const { url } = pressKit.file;
-
-  return (
-    <Flex role="button" overflow="hidden">
-      <S.Button target="_blank" href={url}>
-        Presskit
-      </S.Button>
-      <S.StyloOne small="small" />
-      <S.StyloFour small="small" />
-    </Flex>
-  );
-};
-
+  <Flex role="button" overflow="hidden">
+    <S.Button target="_blank">Presskit</S.Button>
+    <S.StyloOne small="small" />
+    <S.StyloFour small="small" />
+  </Flex>
+);
 export default ButtonBlue;
